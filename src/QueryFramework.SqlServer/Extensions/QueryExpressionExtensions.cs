7adcf2b26da2f15422537f4981c3b37dda2ed1ac
Paul Deen <paul.deen@dutchworkz.nl>
2021-12-12 15:18:29 +0100

    Minor: Improved nesting functions by using {0} instead of field name

--- src/QueryFramework.SqlServer/Extensions/QueryExpressionExtensions.cs
+++ src/QueryFramework.SqlServer/Extensions/QueryExpressionExtensions.cs
@@ -10,45 +10,45 @@ namespace QueryFramework.SqlServer.Extensions
         #region Built-in Sql functions
         /// <summary>Gets the length of this expression.</summary>
         public static IQueryExpression Len(this IQueryExpression instance)
-            => instance.With(expression: instance.FieldName == instance.Expression ? "LEN({0})" : "LEN(" + instance.Expression + ")");
+            => instance.With(expression: instance.FieldName == instance.Expression ? "LEN({0})" : "LEN(" + instance.Expression.Replace($"({instance.FieldName})", "({0})") + ")");
 
         /// <summary>Trims the value of this expression.</summary>
         public static IQueryExpression Trim(this IQueryExpression instance)
-            => instance.With(expression: instance.FieldName == instance.Expression ? "TRIM({0})" : "TRIM(" + instance.Expression + ")");
+            => instance.With(expression: instance.FieldName == instance.Expression ? "TRIM({0})" : "TRIM(" + instance.Expression.Replace($"({instance.FieldName})", "({0})") + ")");
 
         /// <summary>Gets the upper-cased value of this expression.</summary>
         public static IQueryExpression Upper(this IQueryExpression instance)
-            => instance.With(expression: instance.FieldName == instance.Expression ? "UPPER({0})" : "UPPER(" + instance.Expression + ")");
+            => instance.With(expression: instance.FieldName == instance.Expression ? "UPPER({0})" : "UPPER(" + instance.Expression.Replace($"({instance.FieldName})", "({0})") + ")");
 
         /// <summary>Gets the lower-cased value of this expression.</summary>
         public static IQueryExpression Lower(this IQueryExpression instance)
-            => instance.With(expression: instance.FieldName == instance.Expression ? "LOWER({0})" : "LOWER(" + instance.Expression + ")");
+            => instance.With(expression: instance.FieldName == instance.Expression ? "LOWER({0})" : "LOWER(" + instance.Expression.Replace($"({instance.FieldName})", "({0})") + ")");
 
         /// <summary>Gets the left part of this expression.</summary>
         public static IQueryExpression Left(this IQueryExpression instance, int length)
-            => instance.With(expression: instance.FieldName == instance.Expression ? "LEFT({0}, " + length + ")" : "LEFT(" + instance.Expression + ", " + length + ")");
+            => instance.With(expression: instance.FieldName == instance.Expression ? "LEFT({0}, " + length + ")" : "LEFT(" + instance.Expression.Replace($"({instance.FieldName})", "({0})") + ", " + length + ")");
 
         /// <summary>Gets the right part of this expression.</summary>
         public static IQueryExpression Right(this IQueryExpression instance, int length)
-            => instance.With(expression: instance.FieldName == instance.Expression ? "RIGHT({0}, " + length + ")" : "RIGHT(" + instance.Expression + ", " + length + ")");
+            => instance.With(expression: instance.FieldName == instance.Expression ? "RIGHT({0}, " + length + ")" : "RIGHT(" + instance.Expression.Replace($"({instance.FieldName})", "({0})") + ", " + length + ")");
 
         /// <summary>Gets the year of this date expression.</summary>
         public static IQueryExpression Year(this IQueryExpression instance)
-            => instance.With(expression: instance.FieldName == instance.Expression ? "YEAR({0})" : "YEAR(" + instance.Expression + ")");
+            => instance.With(expression: instance.FieldName == instance.Expression ? "YEAR({0})" : "YEAR(" + instance.Expression.Replace($"({instance.FieldName})", "({0})") + ")");
 
         /// <summary>Gets the month of this date expression.</summary>
         public static IQueryExpression Month(this IQueryExpression instance)
-            => instance.With(expression: instance.FieldName == instance.Expression ? "MONTH({0})" : "MONTH(" + instance.Expression + ")");
+            => instance.With(expression: instance.FieldName == instance.Expression ? "MONTH({0})" : "MONTH(" + instance.Expression.Replace($"({instance.FieldName})", "({0})") + ")");
 
         /// <summary>Gets the day of this date expression.</summary>
         public static IQueryExpression Day(this IQueryExpression instance)
-            => instance.With(expression: instance.FieldName == instance.Expression ? "DAY({0})" : "DAY(" + instance.Expression + ")");
+            => instance.With(expression: instance.FieldName == instance.Expression ? "DAY({0})" : "DAY(" + instance.Expression.Replace($"({instance.FieldName})", "({0})") + ")");
 
         /// <summary>Gets the first non-null value of the specified expressions.</summary>
         public static IQueryExpression Coalesce(this IQueryExpression instance, params IQueryExpression[] innerExpressions)
             => instance.With(expression: instance.FieldName == instance.Expression
-                ? "COALESCE({0}, " + string.Join(", ", innerExpressions.Select(innerExpression => innerExpression.Expression)) + ")"
-                : "COALESCE(" + instance.Expression + ", " + string.Join(", ", innerExpressions.Select(innerExpression => innerExpression.Expression)) + ")");
+                ? "COALESCE({0}, " + string.Join(", ", innerExpressions.Select(innerExpression => innerExpression.Expression.Replace($"({instance.FieldName})", "({0})"))) + ")"
+                : "COALESCE(" + instance.Expression + ", " + string.Join(", ", innerExpressions.Select(innerExpression => innerExpression.Expression.Replace($"({instance.FieldName})", "({0})"))) + ")");
 
         /// <summary>Gets the first non-null value of the specified fields.</summary>
         public static IQueryExpression Coalesce(this IQueryExpression instance, params string[] innerFieldNames)
@@ -56,11 +56,11 @@ namespace QueryFramework.SqlServer.Extensions
 
         /// <summary>Gets the count of this expression.</summary>
         public static IQueryExpression Count(this IQueryExpression instance)
-            => instance.With(expression: instance.FieldName == instance.Expression ? "COUNT({0})" : "COUNT(" + instance.Expression + ")");
+            => instance.With(expression: instance.FieldName == instance.Expression ? "COUNT({0})" : "COUNT(" + instance.Expression.Replace($"({instance.FieldName})", "({0})") + ")");
 
         /// <summary>Gets the sum of this expression.</summary>
         public static IQueryExpression Sum(this IQueryExpression instance)
-            => instance.With(expression: instance.FieldName == instance.Expression ? "SUM({0})" : "SUM(" + instance.Expression + ")");
+            => instance.With(expression: instance.FieldName == instance.Expression ? "SUM({0})" : "SUM(" + instance.Expression.Replace($"({instance.FieldName})", "({0})") + ")");
         #endregion
     }
 }
