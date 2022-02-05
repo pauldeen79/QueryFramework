739acc2b7b7ccff16fb468626cbe1437b654e159
Paul Deen <paul.deen@dutchworkz.nl>
2022-02-05 11:26:21 +0100

    Minor: Refactored C# model generation

--- src/QueryFramework.Abstractions.Tests/CodeGenerationProviders/AbstractionsInterfacesModels.cs
+++ src/QueryFramework.Abstractions.Tests/CodeGenerationProviders/AbstractionsInterfacesModels.cs
@@ -1,62 +1,27 @@
-﻿using ModelFramework.Objects.Builders;
+﻿namespace QueryFramework.Abstractions.Tests.CodeGenerationProviders;
 
-namespace QueryFramework.Abstractions.Tests.CodeGenerationProviders
+public class AbstractionsInterfacesModels : QueryFrameworkCodeGenerationCSharpClassBase
 {
-    public class AbstractionsInterfacesModels : QueryFrameworkCodeGenerationCSharpClassBase
+    public override string Path => "QueryFramework.CodeGeneration\\CodeGenerationProviders";
+
+    public override string DefaultFileName => "QueryFrameworkCSharpClassBase.generated.cs";
+
+    public override bool RecurseOnDeleteGeneratedFiles => false;
+
+    protected override string[] NamespacesToAbbreviate => new[]
     {
-        public override string Path => "QueryFramework.CodeGeneration\\CodeGenerationProviders";
+        "System.Collections.Generic",
+        "ModelFramework.Objects.Builders",
+        "ModelFramework.Objects.Contracts"
+    };
 
-        public override string DefaultFileName => "QueryFrameworkCSharpClassBase.generated.cs";
-
-        public override bool RecurseOnDeleteGeneratedFiles => false;
-
-        public override object CreateModel()
-            => new[]
-            {
-                new ClassBuilder()
-                    .WithNamespace("QueryFramework.CodeGeneration.CodeGenerationProviders")
-                    .WithName("QueryFrameworkCSharpClassBase")
-                    .WithPartial()
-                    .AddUsings(_namespacesToAbbreviate)
-                    .AddMethods(new ClassMethodBuilder()
-                        .WithName("GetModels")
-                        .WithProtected()
-                        .WithStatic()
-                        .WithType(typeof(ITypeBase[]))
-                        .AddLiteralCodeStatements($"return {CreateCode()}.Select(x => x.Build()).ToArray();"))
-            }
-            .Select(x => x.Build())
-            .ToArray();
-
-        private static readonly string[] _namespacesToAbbreviate = new[]
-        {
-            "System.Collections.Generic",
-            "ModelFramework.Objects.Builders",
-            "ModelFramework.Objects.Contracts"
-        };
-
-        private string CreateCode()
-        {
-            var models = new[]
-            {
-                typeof(IQueryCondition),
-                typeof(IQueryExpression),
-                typeof(IQueryExpressionFunction),
-                typeof(IQueryParameter),
-                typeof(IQueryParameterValue),
-                typeof(IQuerySortOrder)
-            }.Select(x => x.ToClassBuilder(new ClassSettings())).ToArray();
-            var serviceCollection = new ServiceCollection();
-            var serviceProvider = serviceCollection
-                .AddCsharpExpressionDumper
-                (
-                    x => x.AddSingleton<IObjectHandlerPropertyFilter, SkipDefaultValuesForModelFramework>()
-                          .AddSingleton<ITypeNameFormatter>(new SkipNamespacesTypeNameFormatter(_namespacesToAbbreviate))
-                )
-                .BuildServiceProvider();
-            var dumper = serviceProvider.GetRequiredService<ICsharpExpressionDumper>();
-
-            return dumper.Dump(models);
-        }
-    }
+    protected override Type[] Models => new[]
+    {
+        typeof(IQueryCondition),
+        typeof(IQueryExpression),
+        typeof(IQueryExpressionFunction),
+        typeof(IQueryParameter),
+        typeof(IQueryParameterValue),
+        typeof(IQuerySortOrder)
+    };
 }
