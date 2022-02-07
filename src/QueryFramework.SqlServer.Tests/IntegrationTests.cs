8dbcdf603ba99f020bca36b9ad5905f94630eded
Paul Deen <paul.deen@dutchworkz.nl>
2022-02-07 20:43:03 +0100

    Introduced default query fieldinfo provider for easier usage, and used this in IntegrationTests class. Also moved some mocks from private fields to c'tor, because they're not used outside the c'tor.

--- src/QueryFramework.SqlServer.Tests/IntegrationTests.cs
+++ src/QueryFramework.SqlServer.Tests/IntegrationTests.cs
@@ -5,38 +5,27 @@ public sealed class IntegrationTests : IDisposable
     private readonly ServiceProvider _serviceProvider;
     private IQueryProcessor CreateSut() => _serviceProvider.GetRequiredService<IQueryProcessor>();
     private readonly Mock<IDatabaseEntityRetriever<TestEntity>> _retrieverMock;
-    private readonly Mock<IQueryExpressionEvaluator> _evaluatorMock;
-    private readonly Mock<IDatabaseEntityRetrieverProvider> _databaseEntityRetrieverProviderMock;
-    private readonly Mock<IPagedDatabaseEntityRetrieverSettingsProvider> _settingsProviderMock;
 
     public IntegrationTests()
     {
         _retrieverMock = new Mock<IDatabaseEntityRetriever<TestEntity>>();
-        _evaluatorMock = new Mock<IQueryExpressionEvaluator>();
-        _databaseEntityRetrieverProviderMock = new Mock<IDatabaseEntityRetrieverProvider>();
+        var evaluatorMock = new Mock<IQueryExpressionEvaluator>();
+        var databaseEntityRetrieverProviderMock = new Mock<IDatabaseEntityRetrieverProvider>();
         var retriever = _retrieverMock.Object;
-        _databaseEntityRetrieverProviderMock.Setup(x => x.TryCreate(It.IsAny<ISingleEntityQuery>(), out retriever))
+        databaseEntityRetrieverProviderMock.Setup(x => x.TryCreate(It.IsAny<ISingleEntityQuery>(), out retriever))
                                             .Returns(true);
         var settings = new PagedDatabaseEntityRetrieverSettings("MyTable", "", "", "", null);
-        _settingsProviderMock = new Mock<IPagedDatabaseEntityRetrieverSettingsProvider>();
+        var settingsProviderMock = new Mock<IPagedDatabaseEntityRetrieverSettingsProvider>();
         IPagedDatabaseEntityRetrieverSettings? result = settings;
-        _settingsProviderMock.Setup(x => x.TryCreate(It.IsAny<ISingleEntityQuery>(), out result))
+        settingsProviderMock.Setup(x => x.TryCreate(It.IsAny<ISingleEntityQuery>(), out result))
                              .Returns(true);
-        var fieldInfoMock = new Mock<IQueryFieldInfo>();
-        fieldInfoMock.Setup(x => x.GetDatabaseFieldName(It.IsAny<string>()))
-                     .Returns<string>(x => x);
-        var queryFieldInfo = fieldInfoMock.Object;
-        var queryFieldInfoProvider = new Mock<IQueryFieldInfoProvider>();
-        queryFieldInfoProvider.Setup(x => x.TryCreate(It.IsAny<ISingleEntityQuery>(), out queryFieldInfo))
-                              .Returns(true);
-
         _serviceProvider = new ServiceCollection()
             .AddQueryFrameworkSqlServer()
             .AddSingleton(_retrieverMock.Object)
-            .AddSingleton(_evaluatorMock.Object)
-            .AddSingleton(_databaseEntityRetrieverProviderMock.Object)
-            .AddSingleton(_settingsProviderMock.Object)
-            .AddSingleton(queryFieldInfoProvider.Object)
+            .AddSingleton(evaluatorMock.Object)
+            .AddSingleton(databaseEntityRetrieverProviderMock.Object)
+            .AddSingleton(settingsProviderMock.Object)
+            .AddSingleton<IQueryFieldInfoProvider, DefaultQueryFieldInfoProvider>()
             .AddSingleton(ctx =>
             {
                 var mock = new Mock<IPagedDatabaseCommandProviderProvider>();
---
+++ src/QueryFramework.SqlServer/DefaultQueryFieldInfoProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace QueryFramework.SqlServer;
+
+/// <summary>
+/// Provider for DefaultQueryFieldInfo.
+/// </summary>
+public class DefaultQueryFieldInfoProvider : IQueryFieldInfoProvider
+{
+    public bool TryCreate(ISingleEntityQuery query, out IQueryFieldInfo? result)
+    {
+        result = new DefaultQueryFieldInfo();
+        return true;
+    }
+}
