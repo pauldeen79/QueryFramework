2643c23c26e542277be02b4bbe724a1202b446b9
Paul Deen <paul.deen@dutchworkz.nl>
2022-02-15 08:17:05 +0100

    Removed unnecessary and duplicate code

--- src/ExpressionFramework.Core.Tests/ExpressionEvaluators/FieldExpressionEvaluatorTests.cs
+++ src/ExpressionFramework.Core.Tests/ExpressionEvaluators/FieldExpressionEvaluatorTests.cs
@@ -7,8 +7,7 @@ public class FieldExpressionEvaluatorTests
     {
         // Arrange
         var valueProviderMock = new Mock<IValueProvider>();
-        var functionEvaluatorMock = new Mock<IFunctionEvaluator>();
-        var sut = new FieldExpressionEvaluator(valueProviderMock.Object, new[] { functionEvaluatorMock.Object });
+        var sut = new FieldExpressionEvaluator(valueProviderMock.Object);
         var expressionMock = new Mock<IExpression>();
         var expressionEvaluatorCallbackMock = new Mock<IExpressionEvaluatorCallback>();
 
@@ -21,12 +20,11 @@ public class FieldExpressionEvaluatorTests
     }
 
     [Fact]
-    public void TryEvaluate_Returns_True_When_Expression_Is_A_FieldExpression_Without_Function()
+    public void TryEvaluate_Returns_True_When_Expression_Is_A_FieldExpression()
     {
         // Arrange
         var valueProviderMock = new Mock<IValueProvider>();
-        var functionEvaluatorMock = new Mock<IFunctionEvaluator>();
-        var sut = new FieldExpressionEvaluator(valueProviderMock.Object, new[] { functionEvaluatorMock.Object });
+        var sut = new FieldExpressionEvaluator(valueProviderMock.Object);
         var expressionMock = new Mock<IFieldExpression>();
         expressionMock.SetupGet(x => x.Function).Returns(default(IExpressionFunction));
         expressionMock.SetupGet(x => x.FieldName).Returns("Test");
@@ -39,29 +37,5 @@ public class FieldExpressionEvaluatorTests
         // Assert
         actual.Should().BeTrue();
         result.Should().Be(12345);
-    }
-
-
-    [Fact]
-    public void TryEvaluate_Returns_True_When_Expression_Is_A_FieldExpression_With_Function()
-    {
-        // Arrange
-        var valueProviderMock = new Mock<IValueProvider>();
-        var functionEvaluatorMock = new Mock<IFunctionEvaluator>();
-        var sut = new FieldExpressionEvaluator(valueProviderMock.Object, new[] { functionEvaluatorMock.Object });
-        var expressionMock = new Mock<IFieldExpression>();
-        var expressionFunctionMock = new Mock<IExpressionFunction>();
-        expressionMock.SetupGet(x => x.Function).Returns(expressionFunctionMock.Object);
-        expressionMock.SetupGet(x => x.FieldName).Returns("Test");
-        object? functionResult = 12345;
-        functionEvaluatorMock.Setup(x => x.TryEvaluate(It.IsAny<IExpressionFunction>(), It.IsAny<object?>(), It.IsAny<IExpressionEvaluatorCallback>(), out functionResult)).Returns(true);
-        var expressionEvaluatorCallbackMock = new Mock<IExpressionEvaluatorCallback>();
-
-        // Act
-        var actual = sut.TryEvaluate(default, expressionMock.Object, expressionEvaluatorCallbackMock.Object, out var result);
-
-        // Assert
-        actual.Should().BeTrue();
-        result.Should().Be(functionResult);
     }
 }
--- src/ExpressionFramework.Core/ExpressionEvaluators/FieldExpressionEvaluator.cs
+++ src/ExpressionFramework.Core/ExpressionEvaluators/FieldExpressionEvaluator.cs
@@ -3,34 +3,13 @@
 public class FieldExpressionEvaluator : IExpressionEvaluator
 {
     private readonly IValueProvider _valueProvider;
-    private readonly IEnumerable<IFunctionEvaluator> _functionEvaluators;
 
-    public FieldExpressionEvaluator(IValueProvider valueProvider, IEnumerable<IFunctionEvaluator> functionEvaluators)
-    {
-        _valueProvider = valueProvider;
-        _functionEvaluators = functionEvaluators;
-    }
+    public FieldExpressionEvaluator(IValueProvider valueProvider) => _valueProvider = valueProvider;
 
     public bool TryEvaluate(object? item, IExpression expression, IExpressionEvaluatorCallback callback, out object? result)
     {
         if (expression is IFieldExpression fieldExpression)
         {
-            if (expression.Function != null)
-            {
-                foreach (var evaluator in _functionEvaluators)
-                {
-                    if (evaluator.TryEvaluate(expression.Function,
-                                              _valueProvider.GetValue(item, fieldExpression.FieldName),
-                                              callback,
-                                              out var functionResult))
-                    {
-                        result = functionResult;
-                        return true;
-                    }
-                }
-                throw new ArgumentOutOfRangeException(nameof(expression), $"Unsupported function: [{expression.Function.GetType().Name}]");
-            }
-
             result = _valueProvider.GetValue(item, fieldExpression.FieldName);
             return true;
         }
