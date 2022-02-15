5c464c7714ec10ae2dbb7f96560f6a4f879cd50a
Paul Deen <paul.deen@dutchworkz.nl>
2022-02-15 19:29:20 +0100

    Added missing extension method

---
+++ src/ExpressionFramework.Core/DomainModel/Builders/FieldExpressionBuilder.cs
@@ -0,0 +1,7 @@
+﻿namespace ExpressionFramework.Core.DomainModel.Builders;
+
+public partial class FieldExpressionBuilder
+{
+    public FieldExpressionBuilder WithFieldName(string fieldName)
+        => this.Chain(x => x.FieldName = fieldName);
+}
