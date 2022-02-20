e3d0aa0c51e1108fc2cac61e7908b9665ec5b304
Paul Deen <paul.deen@dutchworkz.nl>
2022-02-20 20:50:55 +0100

    Minor: Removed unused code

--- src/ExpressionFramework.Abstractions/Extensions/ExpressionExtensions.cs
+++ src/ExpressionFramework.Abstractions/Extensions/ExpressionExtensions.cs
@@ -4,7 +4,4 @@ public static class ExpressionExtensions
 {
     public static string? GetFieldName(this IExpression instance)
         => (instance as IFieldExpression)?.FieldName;
-
-    public static object? GetConstantValue(this IExpression instance)
-        => (instance as IConstantExpression)?.Value;
 }
