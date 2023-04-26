dd526163a056bee81bf9ca75d9c64097aeaab360
Paul Deen <paul.deen@dutchworkz.nl>
2023-04-26 16:47:18 +0200

    Removed some overrides in code generation class that are now supported out of the box

--- src/QueryFramework.CodeGeneration/CodeGenerationProviders/QueryFrameworkCSharpClassBase.cs
+++ src/QueryFramework.CodeGeneration/CodeGenerationProviders/QueryFrameworkCSharpClassBase.cs
@@ -11,23 +11,9 @@ public abstract partial class QueryFrameworkCSharpClassBase : CSharpClassBase
     protected override Type RecordCollectionType => typeof(IReadOnlyCollection<>);
     protected override Type RecordConcreteCollectionType => typeof(ReadOnlyValueCollection<>);
     protected override string SetMethodNameFormatString => string.Empty;
-    protected override string FileNameSuffix => ".template.generated";
     protected override string ProjectName => Constants.ProjectName;
     protected override Type BuilderClassCollectionType => typeof(IEnumerable<>);
     protected override ArgumentValidationType ValidateArgumentsInConstructor => ArgumentValidationType.Shared;
     protected override bool InheritFromInterfaces => true;
-    protected override string RootNamespace => Constants.Namespaces.Abstractions;
     protected override bool UseLazyInitialization => false; // this needs to be disabled, because extension method-based builders currently don't support this
-
-    protected override string FormatInstanceTypeName(ITypeBase instance, bool forCreate)
-    {
-        if (instance.Namespace == Constants.Namespaces.Abstractions || instance.Namespace == Constants.Namespaces.Core)
-        {
-            return forCreate
-                ? $"{Constants.Namespaces.Core}.{instance.Name}"
-                : $"{Constants.Namespaces.Abstractions}.I{instance.Name}";
-        }
-
-        return string.Empty;
-    }
 }
