37b30572c190f7e628a74b903bdc10362227a031
Paul Deen <paul.deen@dutchworkz.nl>
2023-05-15 16:53:14 +0200

    Got code generation working through t4.plus dotnet global tool

--- src/QueryFramework.CodeGeneration/CodeGenerationProviders/QueryFrameworkCSharpClassBase.cs
+++ src/QueryFramework.CodeGeneration/CodeGenerationProviders/QueryFrameworkCSharpClassBase.cs
@@ -6,6 +6,11 @@ public abstract partial class QueryFrameworkCSharpClassBase : CSharpClassBase
     public override bool RecurseOnDeleteGeneratedFiles => false;
     public override string DefaultFileName => string.Empty; // not used because we're using multiple files, but it's abstract so we need to fill ilt
 
+    public override void Initialize(bool generateMultipleFiles, bool skipWhenFileExists, string basePath)
+    {
+        base.Initialize(true, false, basePath);
+    }
+
     protected override bool CreateCodeGenerationHeader => true;
     protected override bool EnableNullableContext => true;
     protected override Type RecordCollectionType => typeof(IReadOnlyCollection<>);
