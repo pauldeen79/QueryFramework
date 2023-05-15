3154ada69a4474776dfa1f9e2af56a57232bab87
Paul Deen <paul.deen@dutchworkz.nl>
2023-05-15 15:59:54 +0200

    Attempt to use T4Plus to generate stuff

--- src/QueryFramework.CodeGeneration/CodeGenerationProviders/AbstractionsBuildersInterfaces.cs
+++ src/QueryFramework.CodeGeneration/CodeGenerationProviders/AbstractionsBuildersInterfaces.cs
@@ -1,7 +1,7 @@
 ﻿namespace QueryFramework.CodeGeneration.CodeGenerationProviders;
 
 [ExcludeFromCodeCoverage]
-public class AbstractionsBuildersInterfaces : QueryFrameworkCSharpClassBase, ICodeGenerationProvider
+public class AbstractionsBuildersInterfaces : QueryFrameworkCSharpClassBase
 {
     public override string Path => $"{Constants.Namespaces.Abstractions}/Builders";
 
--- src/QueryFramework.CodeGeneration/CodeGenerationProviders/AbstractionsExtensionsBuilders.cs
+++ src/QueryFramework.CodeGeneration/CodeGenerationProviders/AbstractionsExtensionsBuilders.cs
@@ -1,7 +1,7 @@
 ﻿namespace QueryFramework.CodeGeneration.CodeGenerationProviders;
 
 [ExcludeFromCodeCoverage]
-public class AbstractionsExtensionsBuilders : QueryFrameworkCSharpClassBase, ICodeGenerationProvider
+public class AbstractionsExtensionsBuilders : QueryFrameworkCSharpClassBase
 {
     public override string Path => $"{Constants.Namespaces.Abstractions}/Extensions";
 
