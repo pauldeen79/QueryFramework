1a1d47845aed7d9e2727134ecb761a0dd47fff2a
Paul Deen <paul.deen@dutchworkz.nl>
2023-05-15 13:54:21 +0200

    Upgraded package references

--- src/QueryFramework.CodeGeneration/Program.cs
+++ src/QueryFramework.CodeGeneration/Program.cs
@@ -20,12 +20,8 @@ internal static class Program
         var settings = new CodeGenerationSettings(basePath, generateMultipleFiles, dryRun);
 
         // Generate code
-        var generationTypeNames = new[] { "Entities", "Builders", "BuilderFactory" };
         var generators = typeof(QueryFrameworkCSharpClassBase).Assembly.GetExportedTypes().Where(x => x.BaseType == typeof(QueryFrameworkCSharpClassBase)).ToArray();
-        var generationTypes = generators.Where(x => x.Name.EndsWithAny(generationTypeNames));
-        var scaffoldingTypes = generators.Where(x => !x.Name.EndsWithAny(generationTypeNames));
-        _ = generationTypes.Select(x => (QueryFrameworkCSharpClassBase)Activator.CreateInstance(x)!).Select(x => GenerateCode.For(settings.ForGeneration(), multipleContentBuilder, x)).ToArray();
-        _ = scaffoldingTypes.Select(x => (QueryFrameworkCSharpClassBase)Activator.CreateInstance(x)!).Select(x => GenerateCode.For(settings.ForScaffolding(), multipleContentBuilder, x)).ToArray();
+        _ = generators.Select(x => (QueryFrameworkCSharpClassBase)Activator.CreateInstance(x)!).Select(x => GenerateCode.For(settings, multipleContentBuilder, x)).ToArray();
 
         // Log output to console
         if (string.IsNullOrEmpty(basePath))
