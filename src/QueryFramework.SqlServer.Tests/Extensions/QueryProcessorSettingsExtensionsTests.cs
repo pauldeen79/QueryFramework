b3e47a1e6e984a9f11ac9c51beedddef2218ac8d
Paul Deen <paul.deen@dutchworkz.nl>
2023-11-20 11:09:33 +0100

    Fixed code smell - the contract does not allow to use null as table name

--- src/QueryFramework.SqlServer.Tests/Extensions/QueryProcessorSettingsExtensionsTests.cs
+++ src/QueryFramework.SqlServer.Tests/Extensions/QueryProcessorSettingsExtensionsTests.cs
@@ -5,12 +5,11 @@ public class QueryProcessorSettingsExtensionsTests
     private const string EntityTypeName = "MyTestEntity";
 
     [Theory]
-    [InlineData(null, EntityTypeName)]
     [InlineData("", EntityTypeName)]
     [InlineData("A", "A")]
     [InlineData("other value", "other value")]
     [InlineData(" ", " ")]
-    public void WithDefaultTableName_Returns_Correct_Result(string? input, string expectedOutput)
+    public void WithDefaultTableName_Returns_Correct_Result(string input, string expectedOutput)
     {
         // Arrange
         var sut = new PagedDatabaseEntityRetrieverSettings(input, "", "", "", null);
