66eff084bcbb59d462ec26af2d8f2bc2f5abe8d5
Paul Deen <paul.deen@dutchworkz.nl>
2024-03-06 06:46:55 +0100

    Removed some work-arounds in previous commit, which need to be added in ClassFramework

--- src/QueryFramework.Abstractions/IBuilder.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace QueryFramework.Abstractions
-{
-    public interface IBuilder<out T>
-    {
-        T Build();
-    }
-}
--- src/QueryFramework.CodeGeneration2/CodeGenerationProviders/CoreBuilders.cs
+++ src/QueryFramework.CodeGeneration2/CodeGenerationProviders/CoreBuilders.cs
@@ -1,6 +1,4 @@
-﻿using CrossCutting.Common.Extensions;
-
-namespace QueryFramework.CodeGeneration2.CodeGenerationProviders;
+﻿namespace QueryFramework.CodeGeneration2.CodeGenerationProviders;
 
 [ExcludeFromCodeCoverage]
 public class CoreBuilders : QueryFrameworkCSharpClassBase
@@ -17,8 +15,5 @@ public class CoreBuilders : QueryFrameworkCSharpClassBase
             GetCoreModels(),
             Constants.Namespaces.CoreBuilders,
             Constants.Namespaces.Core
-        ).Select(x => x.ToBuilder()
-            .With(y => y.Methods.Single(z => z.Name == "Build").WithReturnTypeName(y.Methods.Single(z => z.Name == "Build").ReturnTypeName.Replace(".Core.", ".Abstractions.I")))
-            .Build()
         );
 }
