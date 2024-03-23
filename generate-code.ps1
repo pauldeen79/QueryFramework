d4716fddac23c32955cdfbf4841cee0f60648ee6
Paul Deen <paul.deen@dutchworkz.nl>
2024-03-23 20:00:43 +0100

    Fixed code generation script

--- generate-code.ps1
+++ generate-code.ps1
@@ -1,4 +1,1 @@
-# dotnet run --project src/QueryFramework.CodeGeneration\QueryFramework.CodeGeneration.csproj
-
-dotnet build -c Debug src/QueryFramework.CodeGeneration/QueryFramework.CodeGeneration.csproj
-t4plus assembly -a src/QueryFramework.CodeGeneration/bin/Debug/net8.0/QueryFramework.CodeGeneration.dll -p src/
+dotnet run --project src/QueryFramework.CodeGeneration\QueryFramework.CodeGeneration.csproj
