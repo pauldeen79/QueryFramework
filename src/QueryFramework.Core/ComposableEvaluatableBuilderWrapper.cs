e9d686de210caf67929e325f93d746204b52e182
Paul Deen <paul.deen@dutchworkz.nl>
2024-03-24 10:11:39 +0100

    Added missing extension methods, and unit tests for all operators

--- src/QueryFramework.Core/ComposableEvaluatableBuilderWrapper.cs
+++ src/QueryFramework.Core/ComposableEvaluatableBuilderWrapper.cs
@@ -15,11 +15,59 @@ public class ComposableEvaluatableBuilderWrapper<T> where T : IQueryBuilder
         _combination = combination;
     }
 
+    public T Contains(object? value)
+        => AddFilterWithOperator(new StringContainsOperatorBuilder(), value);
+
+    public T EndsWith(object? value)
+        => AddFilterWithOperator(new EndsWithOperatorBuilder(), value);
+
     public T IsEqualTo(object? value)
         => AddFilterWithOperator(new EqualsOperatorBuilder(), value);
 
+    public T IsGreaterOrEqualThan(object? value)
+        => AddFilterWithOperator(new IsGreaterOrEqualOperatorBuilder(), value);
+
+    public T IsGreaterThan(object? value)
+        => AddFilterWithOperator(new IsGreaterOperatorBuilder(), value);
+
+    public T IsNotNull()
+        => AddFilterWithOperator(new IsNotNullOperatorBuilder());
+
+    public T IsNotNullOrEmpty()
+        => AddFilterWithOperator(new IsNotNullOrEmptyOperatorBuilder());
+
+    public T IsNotNullOrWhiteSpace()
+        => AddFilterWithOperator(new IsNotNullOrWhiteSpaceOperatorBuilder());
+
     public T IsNull()
         => AddFilterWithOperator(new IsNullOperatorBuilder());
+
+    public T IsNullOrEmpty()
+        => AddFilterWithOperator(new IsNullOrEmptyOperatorBuilder());
+
+    public T IsNullOrWhiteSpace()
+        => AddFilterWithOperator(new IsNullOrWhiteSpaceOperatorBuilder());
+
+    public T IsSmallerOrEqualThan(object? value)
+        => AddFilterWithOperator(new IsSmallerOrEqualOperatorBuilder(), value);
+
+    public T IsSmallerThan(object? value)
+        => AddFilterWithOperator(new IsSmallerOperatorBuilder(), value);
+
+    public T DoesNotContain(object? value)
+        => AddFilterWithOperator(new StringNotContainsOperatorBuilder(), value);
+
+    public T DoesNotEndWith(object? value)
+        => AddFilterWithOperator(new NotEndsWithOperatorBuilder(), value);
+
+    public T IsNotEqualTo(object? value)
+        => AddFilterWithOperator(new NotEqualsOperatorBuilder(), value);
+
+    public T DoesNotStartWith(object? value)
+        => AddFilterWithOperator(new NotStartsWithOperatorBuilder(), value);
+
+    public T StartsWith(object? value)
+        => AddFilterWithOperator(new StartsWithOperatorBuilder(), value);
 
     private T AddFilterWithOperator(OperatorBuilder @operator, object? value)
         => _instance.Where(ComposableEvaluatableBuilderHelper.Create(_fieldName, @operator, value, _combination, _startGroup, _endGroup));
