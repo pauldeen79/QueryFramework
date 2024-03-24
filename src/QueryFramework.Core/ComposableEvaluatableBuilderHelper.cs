2414ee0db908e9d6e71fa1fb737666f22f1b57f8
Paul Deen <paul.deen@dutchworkz.nl>
2024-03-24 08:24:58 +0100

    Added support to ComposableEvaluatableBuilderHelper for Combination, StartGroup and EndGroup

--- src/QueryFramework.Core/ComposableEvaluatableBuilderHelper.cs
+++ src/QueryFramework.Core/ComposableEvaluatableBuilderHelper.cs
@@ -2,27 +2,39 @@
 
 public static class ComposableEvaluatableBuilderHelper
 {
-    public static ComposableEvaluatableBuilder Create(string fieldName, OperatorBuilder @operator, object? value)
+    public static ComposableEvaluatableBuilder Create(string fieldName, OperatorBuilder @operator, object? value, Combination? combination = null, bool? startGroup = null, bool? endGroup = null)
         => new ComposableEvaluatableBuilder()
+            .WithCombination(combination)
             .WithLeftExpression(new FieldExpressionBuilder().WithExpression(new ContextExpressionBuilder()).WithFieldName(fieldName))
             .WithOperator(@operator)
-            .WithRightExpression(new ConstantExpressionBuilder().WithValue(value));
+            .WithRightExpression(new ConstantExpressionBuilder().WithValue(value))
+            .WithStartGroup(startGroup)
+            .WithEndGroup(endGroup);
 
-    public static ComposableEvaluatableBuilder Create(ExpressionBuilder leftExpression, OperatorBuilder @operator, object? value)
+    public static ComposableEvaluatableBuilder Create(ExpressionBuilder leftExpression, OperatorBuilder @operator, object? value, Combination? combination = null, bool? startGroup = null, bool? endGroup = null)
         => new ComposableEvaluatableBuilder()
+            .WithCombination(combination)
             .WithLeftExpression(leftExpression)
             .WithOperator(@operator)
-            .WithRightExpression(new ConstantExpressionBuilder().WithValue(value));
+            .WithRightExpression(new ConstantExpressionBuilder().WithValue(value))
+            .WithStartGroup(startGroup)
+            .WithEndGroup(endGroup);
 
-    public static ComposableEvaluatableBuilder Create(string fieldName, OperatorBuilder @operator)
+    public static ComposableEvaluatableBuilder Create(string fieldName, OperatorBuilder @operator, Combination? combination = null, bool? startGroup = null, bool? endGroup = null)
         => new ComposableEvaluatableBuilder()
+            .WithCombination(combination)
             .WithLeftExpression(new FieldExpressionBuilder().WithExpression(new ContextExpressionBuilder()).WithFieldName(fieldName))
             .WithOperator(@operator)
-            .WithRightExpression(new EmptyExpressionBuilder());
+            .WithRightExpression(new EmptyExpressionBuilder())
+            .WithStartGroup(startGroup)
+            .WithEndGroup(endGroup);
 
-    public static ComposableEvaluatableBuilder Create(ExpressionBuilder leftExpression, OperatorBuilder @operator)
+    public static ComposableEvaluatableBuilder Create(ExpressionBuilder leftExpression, OperatorBuilder @operator, Combination? combination = null, bool? startGroup = null, bool? endGroup = null)
         => new ComposableEvaluatableBuilder()
+            .WithCombination(combination)
             .WithLeftExpression(leftExpression)
             .WithOperator(@operator)
-            .WithRightExpression(new EmptyExpressionBuilder());
+            .WithRightExpression(new EmptyExpressionBuilder())
+            .WithStartGroup(startGroup)
+            .WithEndGroup(endGroup);
 }
--- src/QueryFramework.Core/ComposableEvaluatableBuilderWrapper.cs
+++ src/QueryFramework.Core/ComposableEvaluatableBuilderWrapper.cs
@@ -4,11 +4,15 @@ public class ComposableEvaluatableBuilderWrapper<T> where T : IQueryBuilder
 {
     private readonly T _instance;
     private readonly string _fieldName;
+    private readonly Combination? _combination;
+    private bool _startGroup;
+    private bool _endGroup;
 
-    public ComposableEvaluatableBuilderWrapper(T instance, string fieldName)
+    public ComposableEvaluatableBuilderWrapper(T instance, string fieldName, Combination? combination = null)
     {
         _instance = instance.IsNotNull(nameof(instance));
         _fieldName = fieldName.IsNotNull(nameof(fieldName));
+        _combination = combination;
     }
 
     public T IsEqualTo(object? value)
@@ -18,8 +22,20 @@ public class ComposableEvaluatableBuilderWrapper<T> where T : IQueryBuilder
         => AddFilterWithOperator(new IsNullOperatorBuilder());
 
     private T AddFilterWithOperator(OperatorBuilder @operator, object? value)
-        => _instance.Where(ComposableEvaluatableBuilderHelper.Create(_fieldName, @operator, value));
+        => _instance.Where(ComposableEvaluatableBuilderHelper.Create(_fieldName, @operator, value, _combination, _startGroup, _endGroup));
 
     private T AddFilterWithOperator(OperatorBuilder @operator)
-        => _instance.Where(ComposableEvaluatableBuilderHelper.Create(_fieldName, @operator));
+        => _instance.Where(ComposableEvaluatableBuilderHelper.Create(_fieldName, @operator, _combination, _startGroup, _endGroup));
+
+    public ComposableEvaluatableBuilderWrapper<T> WithStartGroup(bool startGroup = true)
+    {
+        _startGroup = startGroup;
+        return this;
+    }
+
+    public ComposableEvaluatableBuilderWrapper<T> WithEndGroup(bool endGroup = true)
+    {
+        _endGroup = endGroup;
+        return this;
+    }
 }
