b359bf7ead16a390283db3099a958c873696f475
Paul Deen <paul.deen@dutchworkz.nl>
2025-02-14 15:00:22 +0100

    Upgraded dependencies

--- src/QueryFramework.CodeGeneration/Program.cs
+++ src/QueryFramework.CodeGeneration/Program.cs
@@ -1,6 +1,4 @@
-﻿using CsharpExpressionDumper.Abstractions;
-
-namespace QueryFramework.CodeGeneration;
+﻿namespace QueryFramework.CodeGeneration;
 
 [ExcludeFromCodeCoverage]
 internal static class Program
@@ -36,22 +34,45 @@ internal static class Program
         using var scope = serviceProvider.CreateScope();
         var engine = scope.ServiceProvider.GetRequiredService<ICodeGenerationEngine>();
 
-        var dumper = scope.ServiceProvider.GetRequiredService<ICsharpExpressionDumper>();
-        var service = scope.ServiceProvider.GetRequiredService<IPipelineService>();
-        var settings = new PipelineSettingsBuilder().WithCopyInterfaces().WithInheritFromInterfaces();
-        var results = new List<TypeBase>();
-        foreach (var type in typeof(Program).Assembly.GetTypes().Where(x => x.Namespace?.StartsWith("QueryFramework.CodeGeneration.Models") == true))
-        {
-            var result = await service.ProcessAsync(new ClassFramework.Pipelines.Reflection.ReflectionContext(type, settings, System.Globalization.CultureInfo.InvariantCulture));
-            results.Add(result.Value!);
-        }
-        var source = dumper.Dump(results);
+        //var dumper = scope.ServiceProvider.GetRequiredService<ICsharpExpressionDumper>();
+        //var service = scope.ServiceProvider.GetRequiredService<IPipelineService>();
+        //var settings = new PipelineSettingsBuilder().WithCopyInterfaces().WithInheritFromInterfaces();
+        //var results = new List<TypeBase>();
+        //foreach (var type in typeof(Program).Assembly.GetTypes().Where(x => x.Namespace?.StartsWith("QueryFramework.CodeGeneration.Models") == true))
+        //{
+        //    var result = await service.ProcessAsync(new ClassFramework.Pipelines.Reflection.ReflectionContext(type, settings, System.Globalization.CultureInfo.InvariantCulture));
+        //    results.Add(result.Value!);
+        //}
+        //var source = dumper.Dump(results);
+
         // Generate code
-        await Task.WhenAll(generators
-            .Select(x => (ICodeGenerationProvider)scope.ServiceProvider.GetRequiredService(x))
-            .Select(x => engine.Generate(x, new MultipleStringContentBuilderEnvironment(), new CodeGenerationSettings(basePath, Path.Combine(x.Path, $"{x.GetType().Name}.template.generated.cs")))));
+        foreach (var generatorType in generators)
+        {
+            var generator = (CsharpClassGeneratorCodeGenerationProviderBase)scope.ServiceProvider.GetRequiredService(generatorType);
+            var result = await engine.Generate(generator, new MultipleStringContentBuilderEnvironment(), new CodeGenerationSettings(basePath, Path.Combine(generator.Path, $"{generatorType.Name}.template.generated.cs"))).ConfigureAwait(false);
+            if (!result.IsSuccessful())
+            {
+                Console.WriteLine("Errors:");
+                WriteError(result);
+                break;
+            }
+        }
 
         // Log output to console
         Console.WriteLine($"Code generation completed, check the output in {basePath}");
+    }
+
+    private static void WriteError(Result error)
+    {
+        Console.WriteLine($"{error.Status} {error.ErrorMessage}");
+        foreach (var validationError in error.ValidationErrors)
+        {
+            Console.WriteLine($"{string.Join(",", validationError.MemberNames)}: {validationError.ErrorMessage}");
+        }
+
+        foreach (var innerResult in error.InnerResults)
+        {
+            WriteError(innerResult);
+        }
     }
 }
